5975ece7330c05dd6c7a5ea1b155f2448e9f171b
DominikWro <dominik.wronski@gmail.com>
2018-04-03 05:13:43 +0200

    Block class

--- src/main/kotlin/Block.kt
+++ src/main/kotlin/Block.kt
@@ -1,2 +1,14 @@
-class Block {
+import java.util.Date;
+
+class Block(
+        val hash: String,
+        val previousHash: String,
+        val data: String,
+        val timeStamp: Long
+){
+    fun Block(){
+      val data = this.data
+      val previousHash = this.previousHash
+      val timeStamp = this.timeStamp.toByte()
+    }
 }
