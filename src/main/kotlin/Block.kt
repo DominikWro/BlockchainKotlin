2e1b30b317702d451204eb5bf25118748abcfaab
DominikWro <dominik.wronski@gmail.com>
2018-04-04 07:35:13 +0200

    Block methods

--- src/main/kotlin/Block.kt
+++ src/main/kotlin/Block.kt
@@ -1,12 +1,45 @@
+
 class Block(
-        val hash: String,
-        val previousHash: String,
         val data: String,
-        val timeStamp: Long
+        val previousHash: String,
+        val timeStamp: Long,
+        val hash: calculateHash()
 ){
+
     fun Block(){
       val data = this.data
       val previousHash = this.previousHash
       val timeStamp = this.timeStamp.toByte()
+    }
+
+    fun calculateHash(): String {
+        val calculatedhash = StringUtil.applySha256(
+                this.previousHash +
+                        java.lang.Long.toString(this.timeStamp) +
+                        this.data
+        )
+        return calculatedhash
+    }
+
+    fun isChainValid(): Boolean {
+        var currentBlock: Block
+        var previousBlock: Block
+
+        //loop through blockchain to check hashes:
+        for (i in 1 until blockchain.size()) {
+            currentBlock = blockchain.get(i)
+            previousBlock = blockchain.get(i - 1)
+            //compare registered hash and calculated hash:
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                println("Current Hashes not equal")
+                return false
+            }
+            //compare previous hash and registered previous hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                println("Previous Hashes not equal")
+                return false
+            }
+        }
+        return true
     }
 }
